2a136dfb04c555aaacb4a9f6910176d26d53dc72
Ou Wang <owang01@yahoo.com>
2020-06-19 11:00:09 -0700

    Remove colons from filenames

---
+++ sla/RADS/call_rads_noice_mad_gfofile-170_20_-10_9215.m
@@ -0,0 +1,15 @@
+% To run this script in background:  
+% matlab -nodesktop -nodisplay < call_rads_noice_mad_gfofile-170_20_-10_9215.m >&! call_rads_noice_mad_gfofile-170_20_-10_9215.log&
+
+startup_v4_gcmfaces;
+
+choiceData='gfofile';doTesting=0;l0list=[-170:20:-10];L0list=[-80:20:80];
+for l0=l0list;
+l0
+for L0=L0list;
+L0
+  rads_noice_mad(choiceData,l0,L0)
+end
+end
+disp('END OF JOB')
+exit
